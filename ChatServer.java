7d62eef9d4e07317e42382ee8d364708df0871df
sojung <sojung@LAPTOP-QOKPPUK5>
2019-05-24 12:36:01 +0900

    run for warring word and don't repeat.

--- ChatServer.java
+++ ChatServer.java
@@ -33,7 +33,7 @@ class ChatThread extends Thread{
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			id = br.readLine();
-			broadcast(id + " entered.");
+			broadcast(id + " entered.", id);
 			System.out.println("[Server] User (" + id + ") entered.");
 			synchronized(hm){
 				hm.put(this.id, pw);
@@ -50,9 +50,11 @@ class ChatThread extends Thread{
 				if(line.equals("/quit"))
 					break;
 				if(line.indexOf("/to ") == 0){
-					sendmsg(line);
+					sendmsg(line, id);
+				}else if(line.indexOf("/userlist") == 0){
+					send_userlist(id);
 				}else
-					broadcast(id + " : " + line);
+					broadcast(id + " : " + line, id);
 			}
 		}catch(Exception ex){
 			System.out.println(ex);
@@ -60,36 +62,107 @@ class ChatThread extends Thread{
 			synchronized(hm){
 				hm.remove(id);
 			}
-			broadcast(id + " exited.");
+			broadcast(id + " exited.", id);
+			System.out.println("[Server] User (" + id + ") exited.");
 			try{
 				if(sock != null)
 					sock.close();
 			}catch(Exception ex){}
 		}
 	} // run
-	public void sendmsg(String msg){
+	public void sendmsg(String msg, String user){
+		Object warn = hm.get(user);
 		int start = msg.indexOf(" ") +1;
 		int end = msg.indexOf(" ", start);
 		if(end != -1){
 			String to = msg.substring(start, end);
 			String msg2 = msg.substring(end+1);
-			Object obj = hm.get(to);
-			if(obj != null){
-				PrintWriter pw = (PrintWriter)obj;
-				pw.println(id + " whisphered. : " + msg2);
-				pw.flush();
-			} // if
+			if((warn != null) && warring(msg)) {
+				PrintWriter user_pw = (PrintWriter)warn;
+				user_pw.println("You used warring word, so Your message can't send to other users.");
+				user_pw.flush();
+			}
+			else {
+				Object obj = hm.get(to);
+				if(obj != null){
+					PrintWriter pw = (PrintWriter)obj;
+					pw.println(id + " whisphered. : " + msg2);
+					pw.flush();
+				} // if
+			}
 		}
 	} // sendmsg
-	public void broadcast(String msg){
-		synchronized(hm){
-			Collection collection = hm.values();
-			Iterator iter = collection.iterator();
-			while(iter.hasNext()){
-				PrintWriter pw = (PrintWriter)iter.next();
-				pw.println(msg);
-				pw.flush();
+	public void broadcast(String msg, String user){
+		
+		Object obj = hm.get(user);
+		if( (obj != null) && warring(msg)) {
+			PrintWriter user_pw = (PrintWriter)obj; 
+			user_pw.println("You used warring word, so Your message can't send to other users.");
+			user_pw.flush();
+		}
+		else {
+			synchronized(hm){
+				Collection collection = hm.values();
+				Iterator iter = collection.iterator();
+				PrintWriter user_pw = (PrintWriter)obj;
+				if(obj == null) { //null doesn't need to pw_user
+					while(iter.hasNext()){
+						PrintWriter pw = (PrintWriter)iter.next();
+						pw.println(msg);
+						pw.flush();
+					}
+				}
+				else {
+					while(iter.hasNext()){
+						PrintWriter pw = (PrintWriter)iter.next();
+						if(user_pw == pw) {
+							//no send message
+						}else {
+							pw.println(msg);
+							pw.flush();
+						}
+					}
+				}
 			}
 		}
 	} // broadcast
+	//send to user name for run method ,then user is not null
+	//convert Printwriter to user's pw
+	//Check if pw and user's pw are same ,then no println
+	
+	public void send_userlist(String user) {
+		int count = 0;
+		Object obj = hm.get(user);
+		PrintWriter pw = (PrintWriter)obj;
+		
+		Iterator<String>list = hm.keySet().iterator();
+		while(list.hasNext()) {
+			String clinet = list.next();
+			count++;
+			pw.println(clinet);
+			pw.flush();
+		}
+		pw.println("userlist : " + count);
+		pw.flush();
+	}//userlist Method
+	//send to user name for run method
+	//save to user's printwriter to Object and convert Printwriter
+	//hm.keyset to iterator and loop
+	//Make a String key and put the hm.key
+	//Printout key 
+	
+	public boolean warring(String line) {
+		ArrayList<String>word = new ArrayList<String>();
+		word.add("bitch");
+		word.add("idiot");
+		word.add("What the hell");
+		word.add("fuck");
+		word.add("shit");
+		
+		if(line.contains(word.get(0))||line.contains(word.get(1))||line.contains(word.get(2))||line.contains(word.get(3))||line.contains(word.get(4))) {
+			return true;
+		}
+		
+		return false;
+	}
 }
